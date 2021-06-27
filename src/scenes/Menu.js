6b6a0b222e9206fc740273531445e53ff02cc438
SeanJTran <sejtran@ucsc.edu>
2021-06-26 20:50:51 -0700

    made some changes

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -4,7 +4,7 @@ class Menu extends Phaser.Scene {
     }
     preload() {
         // load audio
-        this.load.image('field', './assets/Landscape2.png');
+        this.load.image('field', './assets/Landscape 3.png');
         this.load.audio('sfx_select', './assets/blip_select12.wav');
         this.load.audio('sfx_explosion', './assets/explosion38.wav');
         this.load.audio('sfx_rocket', './assets/rocket_shot.wav');
@@ -24,11 +24,11 @@ class Menu extends Phaser.Scene {
         }
         this.field = this.add.tileSprite(0, 0, 640, 480, 'field').setOrigin(0, 0);
         // show menu text
-        this.add.text(game.config.width/2, game.config.height/2 - borderUISize - borderPadding, 'DragonStrike', menuConfig).setOrigin(0);
-        this.add.text(game.config.width/2, game.config.height/2, 'Use ←→ arrows to move & (F) to fire', menuConfig).setOrigin(0);
-        menuConfig.backgroundColor = '#00FF00';
-        menuConfig.color = '#000';
-        this.add.text(game.config.width/2, game.config.height/2 + borderUISize + borderPadding, 'Press ← for Novice or → for Expert', menuConfig).setOrigin(0);
+        //this.add.text(game.config.width/2, game.config.height/2 - borderUISize - borderPadding, 'DragonStrike', menuConfig).setOrigin(0.5);
+        //this.add.text(game.config.width/2, game.config.height/2, 'Use ←→ arrows to move & (F) to fire', menuConfig).setOrigin(0.5);
+        //menuConfig.backgroundColor = '#00FF00';
+        //menuConfig.color = '#000';
+        //this.add.text(game.config.width/2, game.config.height/2 + borderUISize + borderPadding, 'Press ← for Novice or → for Expert', menuConfig).setOrigin(0.5);
         // define keys
         keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
         keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
