86ff4fba1f2968b9e8df1b41a1460003ee562deb
SeanJTran <sejtran@ucsc.edu>
2021-06-27 19:51:47 -0700

    Final Commit

--- src/main.js
+++ src/main.js
@@ -1,3 +1,22 @@
+/*
+Sean Tran
+DragonStrike
+6/27/2021
+Project Completion Time: 11-13 hours
+
+Point Breakdown
+Finishing the Tutorial: an automatic 20 points.
+I finished the tutorial.
+
+Redesign the game's artwork, UI, and sound to change its theme/aesthetic (to something other than sci-fi) (60):
+I redesigned the game's theme/aesthetic to fantasy with original sounds and drawings that I made.
+
+Create new artwork for all of the in-game assets (rocket, spaceships, explosion) (20):
+I created new sprites for the rocket and spaceships. I also made a new explosion animation.
+
+TOTAL POINTS: 20 + 60 + 20 = 100
+*/
+
 let config = {
     type: Phaser.AUTO,
     width: 640,
